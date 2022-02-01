d82f555758d493d6fa03a5393a4528083972b5b2
Vangdale <geo.kinder@gmail.com>
2022-02-01 09:42:04 +0100

    commit rubricas Signed-off-by: Vangdale <geo.kinder@gmail.com>

---
+++ Bloque5_Rubrica3/Spring4Examples/src/main/java/com/technicalkeeda/Spring4Examples/Spring4ExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.technicalkeeda.Spring4Examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4ExamplesApplication.class, args);
+	}
+
+}
